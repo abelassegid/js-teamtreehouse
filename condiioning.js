85f15b2b64eb479366c51b6390c4d314db3f4d67
abelassegid <abelassegid9@gmail.com>
2025-06-30 12:15:42 +0300

    git comanding

---
+++ condiioning.js
@@ -0,0 +1,8 @@
+const answer = prompt('what is the nearest planet to the sun?')
+
+if (answer.toUpperCase() === "MERCURY"){
+    console.log("you are wright");
+}
+else{
+    console.log("you are wrong");
+}
---
+++ sout.js
@@ -0,0 +1,5 @@
+const msg =prompt("what is there to shout for");
+const myText = msg.toUpperCase();
+const myAll= `your shout is ${myText}`;
+
+document.querySelector('main') .innerHTML = myAll;````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
