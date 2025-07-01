e794f810342924d590c14056d416942f68d72edd
abelassegid <abelassegid9@gmail.com>
2025-07-01 13:19:18 +0300

    new lessons

--- condiioning.js
+++ condiioning.js
@@ -1,8 +1,42 @@
-const answer = prompt('what is the nearest planet to the sun?')
+// const answer = prompt('what is the nearest planet to the sun?')
 
-if (answer.toUpperCase() === "MERCURY"){
-    console.log("you are wright");
-}
-else{
-    console.log("you are wrong");
+// if (answer.toUpperCase() === "MERCURY"){
+//     console.log("you are wright");
+// }
+// else{
+//     console.log("you are wrong");
+// }
+
+// let spaceShips =0;
+// if(spaceShips === 0){
+//     alert('game over !')
+// } else{
+//     alert('keep it going')
+// }
+
+
+// let correctGuess = false;
+// const number = 6;
+// const guess =prompt('Guess a number between 1 and 10');
+
+// if ( +guess === number){
+//     correctGuess = true;
+// }
+
+// if (correctGuess) {
+//     console.log ('you guessed the number!');
+// } else {
+//     console.log(`sorry. The number was ${number}.`)
+// }
+
+const weather = 'snow';
+
+if (weather === 'sun'){
+    console.log("it's sunny, so i'm going swimming.");
+} else if (weather === 'rain') {
+    console.log("it's raining so i will read a book");
+} else if(weather === 'snow'){
+    console.log("it's snowing, so i'm going sliding");
+} else{
+    console.log('i dont know what am doing today');
 }
