964193472e574dc3bb46e9cf05255db7a3a6e8db
abelassegid <abelassegid9@gmail.com>
2025-08-04 12:50:58 +0300

    new lessons about loops

---
+++ do-while.js
@@ -0,0 +1,13 @@
+// function myNumber(first){
+//   return Math.floor(Math.random() * first ) +1;
+// }
+
+// let circle = 0 ;
+// do{
+//     console.log( myNumber(10));
+//     circle += 1;
+// } while(circle < 10);
+do {
+  console.log('Hello');
+} while (false);
+console.log('Goodbye');
---
+++ while.js
@@ -0,0 +1,11 @@
+function myNumber(first){
+  return Math.floor(Math.random() * first ) +1;
+}
+
+
+
+let circle = 0;
+while( circle < 10 ){
+console.log( myNumber(10));
+circle+=1;
+}
